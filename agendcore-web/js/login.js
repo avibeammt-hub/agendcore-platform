c205c440441aa0b87efc496294b6733333ce9be4
Andre <avibeammt@gmail.com>
2026-05-19 07:11:56 -0500

    Loading

--- agendcore-web/js/login.js
+++ agendcore-web/js/login.js
@@ -3,8 +3,9 @@ const API_BASE = 'https://agendcore-platform.onrender.com/api';
 const formLogin = document.getElementById('formLogin');
 const usuarioInput = document.getElementById('usuario');
 const claveInput = document.getElementById('clave');
-const rolInput = document.getElementById('rol');
+//const rolInput = document.getElementById('rol');
 const mensajeLogin = document.getElementById('mensajeLogin');
+const loaderSistema = document.getElementById('loaderSistema');
 const btnVerClave = document.getElementById('btnVerClave');
 
 btnVerClave.addEventListener('click', () => {
@@ -17,12 +18,14 @@ btnVerClave.addEventListener('click', () => {
 
 formLogin.addEventListener('submit', async (e) => {
   e.preventDefault();
-
+  
+  loaderSistema.classList.remove('d-none');
   const usuario = usuarioInput.value.trim();
   const clave = claveInput.value.trim();
 
   if (!usuario || !clave) {
     mostrarMensaje('Ingrese usuario y contraseña.', 'danger');
+	loaderSistema.classList.add('d-none');
     return;
   }
 
@@ -37,18 +40,21 @@ formLogin.addEventListener('submit', async (e) => {
 
     if (!res.ok || data.estado !== 'OK') {
       mostrarMensaje(data.mensaje || 'Credenciales inválidas.', 'danger');
+	  loaderSistema.classList.add('d-none');
       return;
     }
 
-    if (rolInput.value && rolInput.value !== data.usuario.rol) {
-      mostrarMensaje('El rol seleccionado no corresponde al usuario.', 'danger');
-      return;
-    }
+    //if (rolInput.value && rolInput.value !== data.usuario.rol) {
+      //mostrarMensaje('El rol seleccionado no corresponde al usuario.', 'danger');
+      //return;
+    //}
 
     localStorage.setItem('token', data.token);
     localStorage.setItem('usuarioSesion', JSON.stringify(data.usuario));
+	loaderSistema.classList.add('d-none');
 
-    mostrarMensaje('Ingreso exitoso. Redirigiendo...', 'success');
+    mostrarMensaje('Ingreso exitoso. Inicializando plataforma...', 'success');
+	btnVerClave.disabled = true;
 
     setTimeout(() => {
       window.location.href = 'dashboard.html';
@@ -56,6 +62,7 @@ formLogin.addEventListener('submit', async (e) => {
 
   } catch (error) {
     console.error(error);
+	loaderSistema.classList.add('d-none');
     mostrarMensaje('No fue posible conectar con el backend.', 'danger');
   }
 });
