10155d5c484aff67ff3e3b4d6f8fa9c5e8ade3c2
Andre <avibeammt@gmail.com>
2026-05-19 20:47:23 -0500

    Fix IPS Final

--- agendcore-api/src/app.js
+++ agendcore-api/src/app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const cors = require('cors');
 
 const rutasIps = require('./routes/ips.routes');
-const rutasSedes = require('./routes/sedes.routes');
-const rutasProfesionales = require('./routes/profesionales.routes');
-const rutasRolesProfesional = require('./routes/rolesProfesional.routes');
-const rutasServicios = require('./routes/servicios.routes');
-const rutasSalud = require('./routes/salud.routes');
-const rutasAutenticacion = require('./routes/auth.routes');
-const rutasEspecialidades = require('./routes/especialidades.routes');
-const rutasTiposDocumento = require('./routes/tiposDocumento.routes');
-const rutasAgendas = require('./routes/agendas.routes');
+//const rutasSedes = require('./routes/sedes.routes');
+//const rutasProfesionales = require('./routes/profesionales.routes');
+//const rutasRolesProfesional = require('./routes/rolesProfesional.routes');
+//const rutasServicios = require('./routes/servicios.routes');
+//const rutasSalud = require('./routes/salud.routes');
+//const rutasAutenticacion = require('./routes/auth.routes');
+//const rutasEspecialidades = require('./routes/especialidades.routes');
+//const rutasTiposDocumento = require('./routes/tiposDocumento.routes');
+//const rutasAgendas = require('./routes/agendas.routes');
 
 const app = express();
 
@@ -26,14 +26,14 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/ips', rutasIps);
-app.use('/api/sedes', rutasSedes);
-app.use('/api/profesionales', rutasProfesionales);
-app.use('/api/roles-profesional', rutasRolesProfesional);
-app.use('/api/servicios', rutasServicios);
-app.use('/api/salud', rutasSalud);
-app.use('/api/auth', rutasAutenticacion);
-app.use('/api/especialidades', rutasEspecialidades);
-app.use('/api/tipos-documento', rutasTiposDocumento);
-app.use('/api/agendas', rutasAgendas);
+//app.use('/api/sedes', rutasSedes);
+//app.use('/api/profesionales', rutasProfesionales);
+//app.use('/api/roles-profesional', rutasRolesProfesional);
+//app.use('/api/servicios', rutasServicios);
+//app.use('/api/salud', rutasSalud);
+//app.use('/api/auth', rutasAutenticacion);
+//app.use('/api/especialidades', rutasEspecialidades);
+//app.use('/api/tipos-documento', rutasTiposDocumento);
+//app.use('/api/agendas', rutasAgendas);
 
 module.exports = app;
