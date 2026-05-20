ab951f2513636f67f23fcd155b1c1cebd385d4a2
Andre <avibeammt@gmail.com>
2026-05-19 21:37:53 -0500

    Login

--- agendcore-api/src/app.js
+++ agendcore-api/src/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 const rutasIps = require('./routes/ips.routes');
 //const rutasSedes = require('./routes/sedes.routes');
@@ -7,16 +11,14 @@ const rutasIps = require('./routes/ips.routes');
 //const rutasRolesProfesional = require('./routes/rolesProfesional.routes');
 //const rutasServicios = require('./routes/servicios.routes');
 //const rutasSalud = require('./routes/salud.routes');
-//const rutasAutenticacion = require('./routes/auth.routes');
+const rutasAutenticacion = require('./routes/auth.routes');
 //const rutasEspecialidades = require('./routes/especialidades.routes');
 //const rutasTiposDocumento = require('./routes/tiposDocumento.routes');
 //const rutasAgendas = require('./routes/agendas.routes');
 
-const app = express();
 
-app.use(cors());
 
-app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.json({
@@ -31,7 +33,7 @@ app.use('/api/ips', rutasIps);
 //app.use('/api/roles-profesional', rutasRolesProfesional);
 //app.use('/api/servicios', rutasServicios);
 //app.use('/api/salud', rutasSalud);
-//app.use('/api/auth', rutasAutenticacion);
+app.use('/api/auth', rutasAutenticacion);
 //app.use('/api/especialidades', rutasEspecialidades);
 //app.use('/api/tipos-documento', rutasTiposDocumento);
 //app.use('/api/agendas', rutasAgendas);
